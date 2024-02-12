8f6236d7b92fe2a76c2224f94b7208b8a55d99b2
우창진 <deu03161@naver.com>
2024-02-12 16:25:13 +0900

    feat : 파일 업로드 완료 메시지 클라이언트에게 전달 기능 추가

--- src/main/java/com/private_lbs/taskmaster/redis/service/RedisSubService.java
+++ src/main/java/com/private_lbs/taskmaster/redis/service/RedisSubService.java
@@ -11,9 +11,10 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 @Service
+@RequiredArgsConstructor
 public class RedisSubService implements MessageListener {
     private final ObjectMapper mapper = new ObjectMapper();
-    private final SseEmitters sseEmitters= new SseEmitters();
+    private final SseEmitters sseEmitters;
     /**
      * @author boyjo
      * @date 2/3/24
@@ -27,6 +28,7 @@ public class RedisSubService implements MessageListener {
             //TODO: 1. AI 가공이 완료되었다는 메시지가 수신 로직 구현 - SSE 필요
             //TODO: 2. 각 상태별 메시지가 수신 로직 구현 - SSE 필요
             String mes=new String(message.getBody(),"UTF-8");
+//            sseEmitters.addEmitter();
             sseEmitters.sendMessage(mes);
         } catch (RedisException | IOException e) {
             throw new RedisException(RedisErrorCode.MESSAGE_RECEIVE_FAILED);
